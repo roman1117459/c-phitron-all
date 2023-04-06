315fd633b3eb146588cbc9749ad72d701e014df4
roman1117459 <roman1117459@gmail.com>
2023-04-06 08:09:55 +0600

    modified module 3

--- Module 3/break.c
+++ Module 3/break.c
@@ -5,11 +5,12 @@ int main()
     for(i=1;i<=10;i=i+1)
     {
         
-        printf("%d\n",i);
+        
         if(i==5)
         {
             break;
         }
+        printf("%d\n",i);
     }
     return 0;
 }
